78430a4cd4cf2566da91917eefe3b220f477097b
nimeshakya <nimeshakyabtw@gmail.com>
2025-08-25 19:04:20 +0545

    Hello, Triangle!!

--- triangleDrawer.js
+++ triangleDrawer.js
@@ -6,6 +6,8 @@ class TriangleDrawer
 
         this.mvp = gl.getUniformLocation(this.prog, "mvp");
 
+        this.color = gl.getAttribLocation(this.prog, "color");
+
         this.verPos = gl.getAttribLocation(this.prog, "pos");
 
         // create the buffer objects
@@ -18,45 +20,66 @@ class TriangleDrawer
         ];
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexbuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(pos), gl.STATIC_DRAW);
-
-        // edge line buffer
-        this.linebuffer = gl.createBuffer();
-        var line = [
-            0,1, 1,2, 2,0
+        this.colorbuffer = gl.createBuffer();
+        var colors = [
+            1, 0, 0, 1,
+            0, 1, 0, 1,
+            0, 0, 1, 1
         ]
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.linebuffer);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(line), gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.colorbuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+        // edge line buffer
+        // this.linebuffer = gl.createBuffer();
+        // var line = [
+        //     0,1, 1,2, 2,0
+        // ]
+        // gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.linebuffer);
+        // gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(line), gl.STATIC_DRAW);
     }
 
     draw() {
         // draw the line segments
         gl.useProgram(this.prog);
-        gl.uniformMatrix4fv(this.mvp, false, [
+        var viewMatrix = [
             1, 0, 0, 0,
             0, 1, 0, 0,
             0, 0, 1, 0,
             0, 0, 0, 1
-        ])
+        ]
+
+        gl.uniformMatrix4fv(this.mvp, false, viewMatrix)
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexbuffer);
         gl.vertexAttribPointer(this.verPos, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(this.verPos);
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.linebuffer);
-        gl.drawElements(gl.LINES, 6, gl.UNSIGNED_BYTE, 0);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.colorbuffer);
+        gl.vertexAttribPointer(this.color, 4, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(this.color);
+        
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
     }
 }
 
 var triangleVS = `
     attribute vec3 pos;
+    attribute vec4 color;
+
     uniform mat4 mvp;
+    
+    varying vec4 vcolor;
 
     void main() {
         gl_Position = mvp * vec4(pos, 1);
+        vcolor = color;
     }
 `
 
 var triangleFS = `
     precision mediump float;
+
+    varying vec4 vcolor;
+
     void main() {
-        gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+        gl_FragColor = vcolor; 
     }
 `
